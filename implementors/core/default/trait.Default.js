0d4a50931f99c838aabfb3c7af9bab29ca414ecb
Jovansonlee Cesar <ivanceras@gmail.com>
2015-12-28 00:59:48 +0800

    Commiting docs to github pages

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rustorm'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='rustorm/query/struct.Page.html' title='rustorm::query::Page'>Page</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='rustorm/query/struct.Limit.html' title='rustorm::query::Limit'>Limit</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
